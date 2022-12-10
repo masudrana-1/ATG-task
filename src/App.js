dd41a9da3fb6f1d1d963966e217fcdb2582197f9
masudrana-1 <masudrana58855@gmail.com>
2022-12-10 17:53:54 +0600

    added button section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App w-11/12 m-auto">
+    <div className="App">
       <Navbar></Navbar>
       <Header></Header>
+      <Home></Home>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,35 @@
 import React from 'react';
+import { BsFillPeopleFill } from 'react-icons/bs'
 import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <div>
-                <a href="">All Posts(32)</a>
-                <a href="">Article</a>
-                <a href="">Event</a>
-                <a href="">Education</a>
-                <a href="">Job</a>
-            </div>
-            <div>
-                <div>
-                    <select className="select w-full max-w-xs">
-                        <option disabled selected>Write a Post</option>
-                        <option>Homer</option>
-                        <option>Marge</option>
-                        <option>Bart</option>
-                        <option>Lisa</option>
-                        <option>Maggie</option>
-                    </select>
+        <div className='home'>
+            <div className='flex select justify-between items-center'>
+                <div className='flex gap-3'>
+                    <a href="" className='text-black'>All Posts(32)</a>
+                    <a href="">Article</a>
+                    <a href="">Event</a>
+                    <a href="">Education</a>
+                    <a href="">Job</a>
                 </div>
-                <div>
-                    <button>
-                        Join Group
-                    </button>
+                <div className='flex items-center'>
+                    <div>
+                        <select className="select w-full max-w-xs">
+                            <option disabled selected>Write a Post</option>
+                            <option>Homer</option>
+                            <option>Marge</option>
+                            <option>Bart</option>
+                            <option>Lisa</option>
+                            <option>Maggie</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button className='button text-white flex justify-center items-center'>
+                            <BsFillPeopleFill className='icon'></BsFillPeopleFill>
+                            <h1>Join Group</h1>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
